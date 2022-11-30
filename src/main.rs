23c55bb752f15dc2bb70618aa911e454f03aeafc
Ivar Scholten <ivar.scholten@protonmail.com>
2022-11-30 15:04:35 +0100

    rom header parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod cpu;
+mod rom;
 
 fn main() {
+    let rom = rom::Rom::from_file("roms/donkey-kong.nes");
+
     let mut cpu = cpu::CPU::new();
-    cpu.load_program(vec![0xA9, 0x42, 0x00]);
+    cpu.load_program(rom.program_rom);
+
     cpu.run();
 }
